c84a55a6eaee410010daa8e3c4eb133663ab4eb5
svakhilr <svakhilr@gmail.com>
2024-08-22 14:18:36 +0530

    side bar optimisation

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { IoHomeSharp } from "react-icons/io5";
 import { RiAliensFill,RiContactsBook2Fill,RiInformation2Fill } from "react-icons/ri";
 import { ImLocation2 } from "react-icons/im";
 import { FaLocationArrow } from "react-icons/fa";
-
+import { userContext } from '../store/authprovider'
+import { Link } from 'react-router-dom';
 
 
 function Sidebar({istoggle,handletoggle}) {
+  const {isloggedin} = useContext(userContext)
   
- 
   return (
     istoggle?
     (<>
@@ -16,12 +17,15 @@ function Sidebar({istoggle,handletoggle}) {
      <div className='fixed inset-y-0 left-0 z-50 w-60 bg-white shadow-md transform '>
         
         <ul className='pl-5 pt-6 font-semibold'>
-            <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4 '><IoHomeSharp />Home</li>
-            <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><FaLocationArrow/>Trip</li>
+          <Link to='/'><li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4 '><IoHomeSharp />Home</li> </Link>
+          <Link to='/trips'><li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><FaLocationArrow/>Trip</li></Link>
             <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><ImLocation2 />Destination</li>
-            <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiInformation2Fill/>About</li>
+          <Link to='/about-us'><li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiInformation2Fill/>About</li></Link>
             <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiContactsBook2Fill/>Contact</li>
-            <li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiAliensFill/>Profile</li>
+            {isloggedin?
+          <Link to="/profile"><li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiAliensFill/>Profile</li> </Link>:
+          <Link to="/signin"><li className='flex items-center gap-2 cursor-pointer hover:opacity-25 mb-4'><RiAliensFill/>Sign In</li></Link>}
+            
         </ul>
     
     </div>
